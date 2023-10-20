2c326264ab6c27b35ba28d5a45adbe700a521872
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-10-20 14:36:36 +0100

    Added the home page

--- waste_management_system/urls.py
+++ waste_management_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for waste_management_system project.
+URL configuration for shopproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("waste_ma_s.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
